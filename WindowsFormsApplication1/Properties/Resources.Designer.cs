7d112e0276eef32222c0a55aa7d500798caed36a
Kenneth <Kenneth@GHOSTOFWINTERFE>
2017-04-02 03:19:41 +0200

    Beta Version

--- WindowsFormsApplication1/Properties/Resources.Designer.cs
+++ WindowsFormsApplication1/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace WindowsFormsApplication1.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon mainIcon {
+            get {
+                object obj = ResourceManager.GetObject("mainIcon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
